a39b308447db472f5f1d0928d3c0ce37469d332c
MAC <mac@emueshinoMacBook-Air.local>
2022-10-09 15:14:09 +0900

    課題提出_2回目

--- Desktop/js-lesson5/app.js
+++ Desktop/js-lesson5/app.js
@@ -2,10 +2,10 @@ for (let i = 1; i < 51; i++) {
     console.log(i);
     if (i % 10 === 0) {
         console.log('今'+ i + '回ループしました。')
-    } else if(i % 4 === 0) {
-        console.log('4で割れる数です。' + i)
     }
-    if (i === 50){
-    alert( i + '回カウント終わりました');
+    if (i % 4 === 0) {
+        console.log('4で割れる数です。' + i)
+    } else if (i === 50) {
+        alert( i + '回カウント終わりました');
     }
 }
